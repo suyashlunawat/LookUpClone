fc769d9aea25e96314f1657d3634962c961da2aa
suyashlunawat <92525202+suyashlunawat@users.noreply.github.com>
2021-12-13 10:09:22 +0100

    Initial Commit

---
+++ Shared/LookUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookUpApp.swift
+//  Shared
+//
+//  Created by Suyash Lunawat on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LookUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
